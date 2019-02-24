537d6a0f47fcbb8e1052a7a4c846d2d8ccb843c7
Michael Wilson <michael.wilson@fishtech.group>
2019-02-24 01:41:36 -0600

    Adding API Gateway Setup

--- environments/production/main.tf
+++ environments/production/main.tf
@@ -1,5 +1,57 @@
 provider "aws" {
+  version = "~> 1.9"
   region = "${var.region}"
+}
+
+provider "archive" {
+    version = "~> 1.0"
+}
+
+# data "archive_file" "lambda" {
+#     type = "zip"
+#     source_file = "../../build/handler.js"
+#     output_path = "../../build/handler.zip"
+# }
+
+
+resource "aws_api_gateway_rest_api" "IncomeStatAPI" {
+  name        = "IncomeStatAPI"
+  description = "This is my API for demonstration purposes"
+}
+
+resource "aws_api_gateway_resource" "balance" {
+  rest_api_id = "${aws_api_gateway_rest_api.IncomeStatAPI.id}"
+  parent_id   = "${aws_api_gateway_rest_api.IncomeStatAPI.root_resource_id}"
+  path_part   = "test"
+}
+
+resource "aws_api_gateway_method" "getBalance" {
+  rest_api_id   = "${aws_api_gateway_rest_api.IncomeStatAPI.id}"
+  resource_id   = "${aws_api_gateway_resource.balance.id}"
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "balanceIntegration" {
+  rest_api_id = "${aws_api_gateway_rest_api.IncomeStatAPI.id}"
+  resource_id = "${aws_api_gateway_resource.balance.id}"
+  http_method = "${aws_api_gateway_method.getBalance.http_method}"
+  type        = "MOCK"
+}
+
+resource "aws_api_gateway_deployment" "incomeStatDeployment" {
+  depends_on = ["aws_api_gateway_integration.balanceIntegration"]
+
+  rest_api_id = "${aws_api_gateway_rest_api.IncomeStatAPI.id}"
+  stage_name  = "test"
+
+  variables = {
+    "answer" = "42"
+  }
+}
+
+data "aws_ssm_parameter" "income_stat_api_key" {
+    name = "/income_stat_api_key"
 }
 
 resource "aws_s3_bucket" "state-file-bucket" {
